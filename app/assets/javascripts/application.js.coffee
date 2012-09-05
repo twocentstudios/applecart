a35bd8e5c200625bc5af5be21a4c8663ab7ebc60
Christopher Trott <chris@twocentstudios.com>
2012-09-05 18:54:57 -0500

    AppDrone: applecart

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,6 @@
+//= require jquery
+//= require jquery_ujs
+//= require init
+//= require jquery.html5-placeholder-shim
+//= require jquery_nested_form
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Applecart::Application.config.secret_token = '1d2002773a0abb3d9d59a6c68b1b80ff29019e44041e3e9e7473d87d1f5609968ae8f156585025a545c899b5a228cac52e073948f0d6c62023e23640787a4940'
