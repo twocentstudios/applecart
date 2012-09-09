322b994d235886bad9fb79c608cf7b685aeb5def
Christopher Trott <chris@twocentstudios.com>
2012-09-08 23:57:42 -0500

    Refactor add item from items index. Put more restrictions on when apples can be added.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,22 +13,24 @@ class ItemsController < ApplicationController
   def add_to_order
     @item = Item.find(params[:item_id])
     @order = current_user.order
-    @order_item = @order.order_items.where("item_id = ?", @item.id).first
-    if @order_item.nil?
-      @order_item = @order.order_items.create
-      @order_item.item = @item
-      @order_item.quantity = 1
-    else
-      logger.debug @order_item.inspect
-      @order_item.quantity = @order_item.quantity + 1
-    end
+    if @order.open?
+      @order_item = @order.order_items.where("item_id = ?", @item.id).first
+      if @order_item.nil?
+        @order_item = @order.order_items.create
+        @order_item.item = @item
+        @order_item.quantity = 1
+      else
+        logger.debug @order_item.inspect
+        @order_item.quantity = @order_item.quantity + 1
+      end
 
-    if @order_item.save!
-      @flash_key = 'success'
-      @flash_value = "You now have #{@order_item.quantity} of the #{@item.name} in your cart"
+      if @order_item.save
+        @flash = { :success => "You now have #{@order_item.quantity} of the #{@item.name} in your cart" }
+      else
+        @flash = { :error => "There was an error and the apple could not be added to your cart" }
+      end
     else
-      @flash_key = 'error'
-      @flash_value = "There was an error and the apple could not be added to your cart"
+      @flash = { :error => "Your order is already being processed"}
     end
 
     respond_to do |format|
--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -1,2 +1,7 @@
 module ItemHelper
+	def add_apple_action(item, order)
+		if order.open?
+			link_to 'Add to cart', item_add_to_order_path(item), :class => 'btn btn-primary', :method => 'POST', :remote => true
+		end
+	end
 end
