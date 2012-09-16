f01c21e3d446c148589b64a36ceb75f7ac314958
Christopher Trott <chris@twocentstudios.com>
2012-09-15 19:10:23 -0500

    Moved admin authentication method to top level app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def authenticate_admin
+		unless current_user.is_admin?
+			flash[:error] = "You're not allowed to do/see this :("
+			redirect_to root_path
+		end
+	end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -106,10 +106,4 @@ class OrdersController < ApplicationController
 		return current_user.order == order || current_user.is_admin?
 	end
 
-	def authenticate_admin
-		unless current_user.is_admin?
-			flash[:error] = "You're not allowed to do/see this :("
-			redirect_to root_path
-		end
-	end
 end
