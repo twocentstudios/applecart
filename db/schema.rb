4e7c4a44131cab085fc38a48bec8edd5bc76852c
Christopher Trott <chris@twocentstudios.com>
2012-09-16 16:19:58 -0500

    Added settings table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120909193423) do
+ActiveRecord::Schema.define(:version => 20120916211248) do
 
   create_table "items", :force => true do |t|
     t.string  "name",                            :null => false
@@ -38,6 +38,17 @@ ActiveRecord::Schema.define(:version => 20120909193423) do
   end
 
   add_index "orders", ["user_id"], :name => "index_orders_on_user_id"
+
+  create_table "settings", :force => true do |t|
+    t.string   "var",                       :null => false
+    t.text     "value"
+    t.integer  "target_id"
+    t.string   "target_type", :limit => 30
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "settings", ["target_type", "target_id", "var"], :name => "index_settings_on_target_type_and_target_id_and_var", :unique => true
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",       :null => false
